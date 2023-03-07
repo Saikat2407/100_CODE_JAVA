6d309ced5a623a3f3587f60de2626b32bb5e7e81
Saikat <s.nayek2407@gmail.com>
2023-03-07 21:51:39 +0530

    Sum of a specified range

---
+++ src/SumInGivenRange.java
@@ -0,0 +1,17 @@
+public class SumInGivenRange {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = rangesolver(0,a,b);
+        System.out.println(sum);
+    }
+
+    static int rangesolver(int sum, int i, int b){
+        if(i > b){
+            return sum;
+        }else{
+            return i + rangesolver(sum,i+1,b);
+        }
+    }
+}
