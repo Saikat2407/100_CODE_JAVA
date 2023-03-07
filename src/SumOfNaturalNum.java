a048d0e108f010b71040968cab032a59c5d07f9e
Saikat <s.nayek2407@gmail.com>
2023-03-07 21:38:58 +0530

    sum of N natural number

---
+++ src/SumOfNaturalNum.java
@@ -0,0 +1,16 @@
+public class SumOfNaturalNum {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = getsumprevious(n);
+
+        System.out.println(sum);
+    }
+
+    static int getsumprevious(int n){
+        if(n==0){
+            return n;
+        }else {
+            return n + getsumprevious(n-1);
+        }
+    }
+}
