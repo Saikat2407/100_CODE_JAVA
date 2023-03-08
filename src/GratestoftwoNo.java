2cd73a17ca9c699490837f5c5d1b32029852589d
Saikat <s.nayek2407@gmail.com>
2023-03-08 11:51:41 +0530

    Gratest of two number

---
+++ src/GratestoftwoNo.java
@@ -0,0 +1,12 @@
+public class GratestoftwoNo {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 50;
+
+        if (a == b){
+            System.out.println("number is equall");
+        }else{
+            System.out.println(Math.max(a,b) + " is gratest among them");
+        }
+    }
+}
