c1de3f23979cd982fae613411b622d7e945a9f71
Saikat <s.nayek2407@gmail.com>
2023-07-23 01:55:21 +0530

    New program

---
+++ src/linearsearch.java
@@ -0,0 +1,15 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int arr[] = inputarray.input();
+        System.out.println(linearsearch(arr,890));
+    }
+
+    public static int linearsearch(int arr[], int key){
+        for (int i = 0; i < arr.length; i++) {
+            if(key == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
