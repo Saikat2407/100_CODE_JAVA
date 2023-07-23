66ce9c9acfcf7a718bedeb819833fcf5da0fa511
Saikat <s.nayek2407@gmail.com>
2023-07-24 00:56:47 +0530

    New program

---
+++ src/sortingthroughbubble.java
@@ -0,0 +1,31 @@
+public class sortingthroughbubble {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4, 1, 10, 30, 40, 20};
+        bubble(arr);
+    }
+
+    static void bubble(int[] arr){
+        int n = arr.length;
+        int temp;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n/2; j++) {
+                if (arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            for (int j = n/2; j < n-1; j++) {
+                if (arr[j] < arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
