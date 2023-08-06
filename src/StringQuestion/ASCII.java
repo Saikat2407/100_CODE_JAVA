172daf42c740c95f522f6adb4eb56fa4f2e05049
Saikat <s.nayek2407@gmail.com>
2023-08-06 18:24:35 +0530

    New program

--- src/StringQuestion/ASCII.java
+++ src/StringQuestion/ASCII.java
@@ -1,4 +1,11 @@
 package StringQuestion;
 
 public class ASCII {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int i = ch;
+
+        System.out.println(ch+ " " + "In ASCII"+ " " + i);
+
+    }
 }
