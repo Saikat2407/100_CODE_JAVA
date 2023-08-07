9e57f24a577b636925b4b90b84725afec1362c0b
Saikat <s.nayek2407@gmail.com>
2023-08-07 22:01:32 +0530

    New program

--- src/StringQuestion/ReverseString.java
+++ src/StringQuestion/ReverseString.java
@@ -1,5 +1,12 @@
 package StringQuestion;
 
 public class ReverseString {
-
+    public static void main(String[] args) {
+        String str = "SAIKAT";
+        String ss = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            ss += str.charAt(i);
+        }
+        System.out.println(ss);
+    }
 }
