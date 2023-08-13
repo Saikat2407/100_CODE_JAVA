03deb6d802767e199c58a6c3d4399dc189788f10
Saikat <s.nayek2407@gmail.com>
2023-08-13 18:06:03 +0530

    New Code added

--- src/StringQuestion/AddNumInString.java
+++ src/StringQuestion/AddNumInString.java
@@ -1,5 +1,21 @@
 package StringQuestion;
 
-public class AddNumInString {
+import java.util.Arrays;
 
+public class AddNumInString {
+    public static void main(String[] args) {
+        String str = "1sa7ik2kal";
+        char[] ch = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int sum = 0;
+
+        for ( char cht: ch) {
+            if (Character.isDigit(cht)){
+                sum += Character.getNumericValue(cht);
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
