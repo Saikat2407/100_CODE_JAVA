f0654f3ec17d722ba69212767b96dc9c32ad4d9e
Saikat <s.nayek2407@gmail.com>
2023-08-13 17:32:22 +0530

    New Code added

--- src/StringQuestion/nonRepeatingChar.java
+++ src/StringQuestion/nonRepeatingChar.java
@@ -1,11 +1,25 @@
 package StringQuestion;
 
+import java.util.ArrayList;
+
 public class nonRepeatingChar {
     public static void main(String[] args) {
-        String str = "Saikat";
-        boolean flag = true;
-        for (char i: str.toCharArray()) {
+        String str = "prepinsta";
+        str = str.toLowerCase();
+        StringBuilder string = new StringBuilder();
 
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            int count = 0;
+            for (int j = 0; j < ch.length; j++) {
+                if(ch[j] == ch[i]){
+                    count++;
+                }
+            }
+            if (count ==1){
+
+            }
         }
+
     }
 }
