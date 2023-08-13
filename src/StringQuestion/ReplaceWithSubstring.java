a7ec5dae10cc2dc333ccdb273a2629703ee0888a
Saikat <s.nayek2407@gmail.com>
2023-08-13 18:47:28 +0530

    New Code added

--- src/StringQuestion/ReplaceWithSubstring.java
+++ src/StringQuestion/ReplaceWithSubstring.java
@@ -1,5 +1,12 @@
 package StringQuestion;
 
 public class ReplaceWithSubstring {
+    public static void main(String[] args) {
+        String str = "SAIKAT NAYEK";
+        String oldstring = "NAYEK";
+        String newstring = "IS A BAD BOY";
+        String Ans = str.replace(oldstring,newstring);
+        System.out.println(Ans);
+    }
 
 }
