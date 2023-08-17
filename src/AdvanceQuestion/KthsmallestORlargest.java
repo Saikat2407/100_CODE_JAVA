4b191c7e9a39329b0aa1ddd3bfa08e774acce670
Saikat <s.nayek2407@gmail.com>
2023-08-17 22:45:56 +0530

    Kth smallest or larget element in an array

--- src/AdvanceQuestion/KthsmallestORlargest.java
+++ src/AdvanceQuestion/KthsmallestORlargest.java
@@ -1,7 +1,22 @@
 package AdvanceQuestion;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class KthsmallestORlargest {
     public static void main(String[] args) {
+        Integer[] arr = new Integer[]{12, 3, 5, 7, 19};
+        int k = 2;
+        System.out.println(kthsmallest(arr,k));
+        System.out.println(kthlargest(arr,k));
+    }
 
+    static int kthsmallest(Integer[] arr, int k){
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+    static int kthlargest(Integer[] arr , int k){
+        Arrays.sort(arr, Collections.reverseOrder());
+        return arr[k-1];
     }
 }
