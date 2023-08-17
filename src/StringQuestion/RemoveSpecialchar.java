31f8141b0103d82ab5e5410116c9dcf38c50444c
Saikat <s.nayek2407@gmail.com>
2023-08-17 21:13:44 +0530

    New Code added

--- src/StringQuestion/RemoveSpecialchar.java
+++ src/StringQuestion/RemoveSpecialchar.java
@@ -11,4 +11,5 @@ public class RemoveSpecialchar {
         }
         System.out.println(ans);
     }
+
 }
--- src/linearsearchInRange.java
+++ src/linearsearchInRange.java
@@ -1,20 +1,43 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class linearsearchInRange {
-    static int Rangesolver(int[] arr, int start, int end, int target){
-        if(arr.length == 0){
+//    static int Rangesolver(int[] arr, int start, int end, int target){
+//        if(arr.length == 0){
+//            return -1;
+//        }
+//        for (int i = start; i <= end ; i++) {
+//            if(arr[i] == target){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    public static int rage(int[] arr, int start, int end, int target){
+        int n = arr.length;
+
+        if(n == 0){
             return -1;
         }
-        for (int i = start; i <= end ; i++) {
-            if(arr[i] == target){
+        for (int i = start; i <= end; i++){
+            if (arr[i] == target){
                 return i;
             }
         }
         return -1;
     }
     public static void main(String[] args) {
-        int[] arr = inputarray.input();
+//        int[] arr = inputarray.input();
+        Scanner sc = new Scanner(System.in);
+        int size = 6;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
         System.out.println(Arrays.toString(arr));
-        System.out.println(Rangesolver(arr,1,3,890));
+        System.out.println(rage(arr,1,size,12));
     }
 }
