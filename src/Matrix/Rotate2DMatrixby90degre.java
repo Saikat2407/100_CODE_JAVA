858dd38ca35e335d4a86664b02648adc217428a0
Saikat <s.nayek2407@gmail.com>
2023-08-29 21:09:08 +0530

    Rotate matrix

---
+++ src/Matrix/Rotate2DMatrixby90degre.java
@@ -0,0 +1,37 @@
+package Matrix;
+
+public class Rotate2DMatrixby90degre {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        rotate(arr);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    static void rotate(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[i].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length/2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][arr.length-1-j];
+                arr[i][arr.length-1-j] = temp;
+            }
+        }
+    }
+}
