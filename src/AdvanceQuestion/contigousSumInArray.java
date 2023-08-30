7dbbdf41b30953b530d3c49c0d72ae2842541cc7
Saikat <s.nayek2407@gmail.com>
2023-08-30 21:14:42 +0530

    effective approach

--- src/AdvanceQuestion/contigousSumInArray.java
+++ src/AdvanceQuestion/contigousSumInArray.java
@@ -3,10 +3,10 @@ package AdvanceQuestion;
 public class contigousSumInArray {
     public static void main(String[] args) {
         int[] arr = new int[]{-2, -3, 4, -1, -3};
-        int ans = SubArraySum(arr);
-        System.out.println(ans);
+        int ans1 = SubArraySum(arr);
+        int ans2 = subArraySum(arr);
+        System.out.println(ans2);
     }
-
     static int SubArraySum(int[] arr){
         int n = arr.length;
         int res = Integer.MIN_VALUE;
@@ -18,5 +18,18 @@ public class contigousSumInArray {
             }
         }
         return res;
+    }
+
+    //O(n)
+    static int subArraySum(int[] arr){
+        int n = arr.length;
+        int maxsum = arr[0];
+        int currentsum = 0;
+
+        for (int i = 0; i < n; i++) {
+            currentsum = Math.max(0, currentsum) + arr[i];
+            maxsum = Math.max(currentsum, maxsum);
+        }
+        return maxsum;
     }
 }
