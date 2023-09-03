f5969d3dae31cb4c849e6bbd715bfc2caa45488a
Saikat <s.nayek2407@gmail.com>
2023-09-03 11:17:20 +0530

    Leetcode problem

---
+++ src/AdvanceQuestion/LongestPalindromeSubString.java
@@ -0,0 +1,13 @@
+package AdvanceQuestion;
+
+import java.util.List;
+
+public class LongestPalindromeSubString {
+    public static void main(String[] args) {
+
+    }
+
+//    static List<List<Integer>> subsetsWithDup(int[] nums){
+//
+//    }
+}
---
+++ src/AdvanceQuestion/ValidParenthesis.java
@@ -0,0 +1,28 @@
+package AdvanceQuestion;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        String sss = "()[]{}";
+        boolean ans = isValid(sss);
+        System.out.println(ans);
+    }
+
+    static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') stack.push(')');
+            else if (ch == '{') {
+                stack.push('}');
+            }
+            else if (ch == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != ch) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
