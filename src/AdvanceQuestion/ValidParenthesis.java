f6a04c476a34bb67fe9a4cdbf580b06efd81fbc8
Saikat <s.nayek2407@gmail.com>
2023-09-03 11:20:23 +0530

    Leetcode problem

--- src/AdvanceQuestion/ValidParenthesis.java
+++ src/AdvanceQuestion/ValidParenthesis.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ValidParenthesis {
     public static void main(String[] args) {
-        String sss = "()[]{}";
+        String sss = "()[]{}[";
         boolean ans = isValid(sss);
         System.out.println(ans);
     }
