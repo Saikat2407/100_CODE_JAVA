d65b933c650e32c56a6305c0df24ca5cea4b756a
Saikat <s.nayek2407@gmail.com>
2023-10-12 22:06:08 +0530

    Imp question Added!

--- src/Array/monster_group.java
+++ src/Array/monster_group.java
@@ -1,5 +1,24 @@
 package Array;
 
 public class monster_group {
+    public static void main(String[] args) {
+        String ss = "PPPPPP@PPP@PP$PP";
+        int ans = solution(ss);
+        System.out.println(ans);
+    }
 
+    public static int solution(String s){
+        int a = 0, ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < s.length(); i++) {
+            a++;
+            if(s.charAt(i) == '@' || s.charAt(i) =='$'){
+                ans = Math.min(a, ans);
+                a = 0;
+            }
+        }
+        ans = Math.min(a,ans);
+
+        return ans;
+    }
 }
